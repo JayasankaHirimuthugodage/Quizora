58d7f66a1b2b3a488fb76f504b92f93c66b1933c
Aenuka Buddhakorala <aenuka.elakollek@gmail.com>
2025-08-18 09:17:09 +0530

    install dependancy

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-// test
+// backend/server.js
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config(); // Load .env variables
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
