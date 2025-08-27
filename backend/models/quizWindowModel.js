402dfa52aac66fc16ba3514eaf1b615c0afec9cb
Chamithued <140527659+Chamithued@users.noreply.github.com>
2025-08-27 08:51:59 +0530

    Quiz window  creation
    Backend for quiz window creation with database connection

--- backend/models/quizWindowModel.js
+++ backend/models/quizWindowModel.js
@@ -1,29 +1,47 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const ProductSchema = mongoose.Schema({
+const WindowSchema = new mongoose.Schema(
+  {
+    degree_title: {
+      type: String,
+      required: [true, "Please enter Degree Title"],
+    },
 
-  name: {
-    type: String,
-    required: [true, "Please Enter Product Name : "],
+    year: {
+      type: Number,
+      required: [true, "Please enter Year"],
+    },
+
+    semester: {
+      type: Number,
+      required: [true, "Please enter Semester"],
+    },
+
+    date: {
+      type: Date,
+      required: [true, "Please enter Date"],
+    },
+
+    start_time: {
+      type: Date,  // stores full Date+Time
+      required: [true, "Please enter Start Time"],
+    },
+
+    end_time: {
+      type: Date,  // stores full Date+Time
+      required: [true, "Please enter End Time"],
+    },
+
+    duration: {
+      type: Number, // minutes or hours
+      required: [true, "Please enter Duration"],
+    },
   },
-
-  quantity: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-
-  price: {
-    type: Number,
-    required: false,
-  },
-},
-{
-    timestamps:true,
-}
-
+  {
+    timestamps: true,
+  }
 );
 
-const Product = mongoose.model("Product", ProductSchema);
+const Window = mongoose.model("Window", WindowSchema);
 
-module.exports = Product;
+export default Window;
