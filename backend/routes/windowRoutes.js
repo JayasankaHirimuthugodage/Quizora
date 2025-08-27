caaa04a1205e2cf73b9a35a4886c94cd00c64ae2
Chamithued <140527659+Chamithued@users.noreply.github.com>
2025-08-27 09:32:09 +0530

    finishin quiz window backend
    added few other api's and routes

--- backend/routes/windowRoutes.js
+++ backend/routes/windowRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 
 router.post('/',windowController.createWindow);
+router.get('/',windowController.getWindows);
 router.get('/:id',windowController.getWindow);
+router.put('/:id',windowController.updateWindow);
+router.delete('/:id',windowController.deleteWindow);
+
 
 export default router;
